0dc0d71f7e60eb2ff06c47b973639c6c13343ed9
Avital <avitalg91@gmail.com>
2019-07-04 17:21:40 +0300

    add dir

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Carousel from './Carousel';
 function App() {
   return (
     <div className="App">
-      <Carousel>
+      <Carousel dir="ltr">
         <div>1</div>
         <div>2</div>
         <div>3</div>
--- src/Carousel.js
+++ src/Carousel.js
@@ -1,51 +1,60 @@
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 import Arrow from './Arrow';
 
 function Carousel(props) {
-   const [currSlide, setCurrSlide] = useState(0)
-   const [maxSlide, setmaxSlide] = useState(0)
-   const [lockNext, setlockNext] = useState(true)
-   const [lockPrev, setlockPrev] = useState(true)
+    const [currSlide, setCurrSlide] = useState(0)
+    const [maxSlide, setmaxSlide] = useState(0)
+    const [lockNext, setlockNext] = useState(true)
+    const [lockPrev, setlockPrev] = useState(true)
+    const [arrowDir, setArrowDir] = useState({ "prev": "left", "next": "right" })
+    useEffect(() => {
+        if (props.children && props.children.length != 0) {
+            setmaxSlide(props.children.length - 1);
+            setlockNext(false);
+        }
 
-   useEffect(() => {
-       if (props.children && props.children.length != 0) {
-           setmaxSlide(props.children.length - 1);
-           setlockNext(false);
-       }
-   }, []);
+        if (props.dir == "rtl") {
+            setArrowDir({ "prev": "right", "next": "left" });
+        }
 
-   useEffect(() => {
-       (maxSlide <= currSlide) ? setlockNext(true) : setlockNext(false);
-       (currSlide > 0) ? setlockPrev(false) : setlockPrev(true);
-   }, [currSlide, maxSlide]);
+    }, []);
 
-   const moveSlide = (side) => {
-       switch (side) {
-           case "prev":
-               if (lockPrev) return;
-               setCurrSlide(currSlide - 1);
-               break;
-           case "next":
-               if (lockNext) return;
-               setCurrSlide(currSlide + 1);
-               break;
-       }
-   }
+    useEffect(() => {
+        (maxSlide <= currSlide) ? setlockNext(true) : setlockNext(false);
+        (currSlide > 0) ? setlockPrev(false) : setlockPrev(true);
+    }, [currSlide, maxSlide]);
 
-   return (
-       <div>
-           <div>
-               <Arrow side="prev" dir="right" lock={lockPrev} click={(e) => moveSlide("prev")} />
-               <Arrow side="next" dir="left" lock={lockNext} click={(e) => moveSlide("next")} />
-           </div>
-           <div>
-               {props.children.map((child, index) => (
-                   (currSlide == index) ? child : null
-               ))}
-           </div>
-       </div>
-   );
+    const moveSlide = (side) => {
+        switch (side) {
+            case "prev":
+                if (lockPrev) return;
+                setCurrSlide(currSlide - 1);
+                break;
+            case "next":
+                if (lockNext) return;
+                setCurrSlide(currSlide + 1);
+                break;
+        }
+    }
+
+    return (
+        <div>
+            <div>
+                <Arrow side="prev" dir={arrowDir.prev} lock={lockPrev} click={(e) => moveSlide("prev")} />
+                <Arrow side="next" dir={arrowDir.next} lock={lockNext} click={(e) => moveSlide("next")} />
+            </div>
+            <CarouselContainer>
+                {props.children.map((child, index) => (
+                    (currSlide == index) ? child : null
+                ))}
+            </CarouselContainer>
+        </div>
+    );
 }
 
 export default Carousel
 
+const CarouselContainer = styled.div`
+text-align: center;
+`
